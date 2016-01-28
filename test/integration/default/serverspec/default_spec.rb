6daa2574684953eb59174d3205e1e17b0e0f7298
John Doe <jdoe@example>
2016-01-28 21:45:46 +0000

    test commit

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -3,7 +3,23 @@ require 'spec_helper'
 describe 'workstation::default' do
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+ # it 'does something' do
+  #  skip 'Replace this with meaningful tests'
+ # end
+
+describe package('tree') do
+ it{ should be_installed }
 end
+
+describe file('/etc/motd') do 
+ it { should be_owned_by 'root' }
+end
+
+describe package('git') do
+ it { should be_installed }
+end
+ 
+end
+
+
+
